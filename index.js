65b4a91440ab41bbfd1a1b346305b788aa784924
KumaranMba <137371588+KumaranMba@users.noreply.github.com>
2024-10-28 02:03:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 import express from 'express'
-import dotenv from 'dotenv'
+import UserRouter from './user.js'
 
-import AppRouter from './routes/index'
-import cors from 'cors'
+const router = express.Router()
+router.get("/", (req, res) => {
+    res.status(200).send(` <h1> Welcome to our Password Reset backend</h1>`);
+  });
+router.use('/user',UserRouter)
 
-
-dotenv.config()
-
-const PORT = process.env.PORT
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-app.use('/',AppRouter)
-
-app.listen(PORT,()=>console.log(`app is listening to port ${PORT}`))
+export default router
