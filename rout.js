bdd9b27a30874a94334a2a58ddec0f014bd9b580
KumaranMba <137371588+KumaranMba@users.noreply.github.com>
2024-10-28 02:37:27 +0530

    Update rout.js

--- rout.js
+++ rout.js
@@ -1,10 +1,1 @@
-import express from 'express'
-import UserRouter from './user.js'
 
-const router = express.Router()
-router.get("/", (req, res) => {
-    res.status(200).send(` <h1> Welcome to our Password Reset backend</h1>`);
-  });
-router.use('/user',UserRouter)
-
-export default router
